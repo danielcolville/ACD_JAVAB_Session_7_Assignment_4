9e8c81b771a49385a0541d6536b278924b89dfe6
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-14 22:31:24 -0400

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,36 +1,36 @@
 package session7;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Anagram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String []str = {"dog","god","odg","cold","clod"};
-		String [] sorted=new String[str.length];
-		for(int i=0;i<str.length;i++) {
-			sorted[i]= new String(selSort(str[i].toCharArray(),str[i].length()));
+		String []str = {"dog","god","cold","clod","listen","silent","state","taste","pot","opt","top"};
+		int []ind=new int[str.length];
+		for(int i=0;i<ind.length;i++) {
+			ind[i]=i;
+		}
+		Word [] sorted=new Word[str.length];
+		for(int i=0;i<str.length;i++) {	
+			String temp=new String(selSort(str[i].toCharArray(),str[i].length()));
+			System.out.println(temp);
+			sorted[i]=new Word(temp,i);
 		}
 		
-		Arrays.sort(sorted);
-		for(int i=0;i<sorted.length;i++) {
-			for(int j=0;j<sorted[i].length();j++) {
-				System.out.print(sorted[i].charAt(j));
-			}
-			System.out.println();
+		Arrays.sort(sorted,new strC());
+		for (int i = 0; i<str.length;i++)  {
+            System.out.print(str[sorted[i].index] + " ");
 		}
-		for(int i=0)
 	
-	}/*
-	public static boolean isAnagram(String s1,String s2) {
-		if(s1.length()!=s2.length()) {
-			return false;
-		}
-		else {
-			
-		}
-	}*/
-
+	}
+	static class strC implements Comparator<Word> {
+		 public int compare(Word a, Word b) 
+	        { 
+	            return a.str.compareTo(b.str); 
+	        } 
+	}
 	
 	public static char[] selSort(char[] sort,int len) {
 		if(len==1) {
@@ -60,5 +60,13 @@ public class Anagram {
 			}
 		}
 		return selSort(next,len-1);
+	}
+	static class Word{
+		public String str;
+		public int index;
+		Word(String str,int index) {
+			this.str=str;
+			this.index=index;
+		}
 	}
 }
